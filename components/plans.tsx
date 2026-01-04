d250931b556a1b768a235dcbf8cdfe0ce4eb1fa8
hloser77 <hamzarahiane@gmail.com>
2026-01-04 21:14:06 +0100

    Update plans.tsx

--- components/plans.tsx
+++ components/plans.tsx
@@ -76,7 +76,7 @@ const plans = [
       "A simple start before upgrading",
     ],
     cta: "Join Free discord",
-    link: "whop.com/hl0ser/universityoftradingma",
+    link: "https://whop.com/hl0ser/universityoftradingma/",
     popular: false,
   },
 ]
