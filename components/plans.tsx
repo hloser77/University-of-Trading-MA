0024c5223b4d162609cc8527a159a8515b258e4a
hloser77 <hamzarahiane@gmail.com>
2026-01-14 16:08:14 +0100

    Update plans.tsx

--- components/plans.tsx
+++ components/plans.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react"
 const plans = [
   {
     name: "1 Month",
-    price: "25",
+    price: "256",
     period: "/month",
     badge: "Starter",
     description: "Begin your journey",
