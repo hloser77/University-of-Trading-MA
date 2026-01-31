4b816df6478ea7e707c319a2bf5dd93920a11319
hloser77 <hamzarahiane@gmail.com>
2026-01-31 16:09:11 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,7 +95,7 @@ export default function RootLayout({
       </head>
 
       <body className="font-sans antialiased">
-        <PromoPopup />
+        {/* <PromoPopup /> */}
         {children}
         <Analytics />
       </body>
